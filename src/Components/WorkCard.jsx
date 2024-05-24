9fd868df9d4f0efa1864d4ea4333bb08c0f437ad
Lokesh_Bijarniya_ <152880035+Lokesh-Bijarniya@users.noreply.github.com>
2024-05-24 19:23:18 +0530

    Update WorkCard.jsx

--- src/Components/WorkCard.jsx
+++ src/Components/WorkCard.jsx
@@ -1,4 +1,6 @@
 import VisibilityIcon from "@mui/icons-material/Visibility";
+import Tilt from "react-parallax-tilt";
+import PropTypes from 'prop-types';
 
 export default function WorkCard({ src, head, description, tech, gitLink }) {
   const colors = [
@@ -10,37 +12,44 @@ export default function WorkCard({ src, head, description, tech, gitLink }) {
   ];
 
   return (
-    <div className="bg-indigo-950 max-w-sm rounded-3xl overflow-hidden">
-      <div className="relative">
-        <img
-          src={src}
-          alt=""
-          className="rounded-t-3xl lg:h-56 w-full transform transition duration-500 hover:scale-110 hover:rotate-3"
-        />
+    <Tilt>
+      <div className="bg-indigo-950 max-w-sm rounded-3xl overflow-hidden">
+        <div className="relative">
+          <img src={src} alt="" className="rounded-t-3xl lg:h-56 w-full" />
 
-        <span className="absolute top-2 right-2">
-          <button className="px-2 bg-yellow-500 rounded-md">
-            <a href={gitLink} target="_blank" className="hover:text-pink-500">
-              <VisibilityIcon />
-            </a>
-          </button>
-        </span>
-      </div>
+          <span className="absolute top-2 right-2">
+            <button className="px-2 bg-yellow-500 rounded-md">
+              <a href={gitLink} target="_blank" className="hover:text-pink-500">
+                <VisibilityIcon />
+              </a>
+            </button>
+          </span>
+        </div>
 
-      <div className="p-4">
-        <h1 className="text-2xl">{head}</h1>
-        <p className="text-violet-300 font-extralight">{description}</p>
-        <h3 className="my-4 flex flex-wrap">
-          {tech.map((item, index) => (
-            <span
-              key={index}
-              className={`mr-2 ${colors[index % colors.length]}`}
-            >
-              {item}
-            </span>
-          ))}
-        </h3>
+        <div className="p-4">
+          <h1 className="text-2xl">{head}</h1>
+          <p className="text-violet-300 font-extralight">{description}</p>
+          <h3 className="my-4 flex flex-wrap">
+            {tech.map((item, index) => (
+              <span
+                key={index}
+                className={`mr-2 ${colors[index % colors.length]}`}
+              >
+                {item}
+              </span>
+            ))}
+          </h3>
+        </div>
       </div>
-    </div>
+    </Tilt>
   );
 }
+
+
+WorkCard.propTypes = {
+  src: PropTypes.string.isRequired,
+  head: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  tech: PropTypes.arrayOf(PropTypes.string).isRequired,
+  gitLink: PropTypes.string.isRequired,
+};
